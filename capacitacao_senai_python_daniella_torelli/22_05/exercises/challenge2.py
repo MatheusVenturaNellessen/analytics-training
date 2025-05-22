07447bda4079b8a4ee641b59c2fd893ca4024d23
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2025-05-22 16:00:51 -0300

    feat: adiciona exercícios 1-8 do challenge2.py ao projeto
    1. Quais são os diretores mais recorrentes? 2. Qual a média das avaliações? 3. Quais os filmes mais antigos e mais recentes? 4. Qual a média de duração dos filmes? 5. Quantos filmes há por década? 6. Qual gênero é mais frequente? 7. Quais os filmes com mais de 3 horas de duração? 8. Quais são os 10 melhores filmes de acordo com as avaliações? E os 10 piores?

---
+++ capacitacao_senai_python_daniella_torelli/22_05/exercises/challenge2.py
@@ -0,0 +1,83 @@
+'''
+1. Quais são os diretores mais recorrentes?
+2. Qual a média das avaliações?
+3. Quais os filmes mais antigos e mais recentes?
+4. Qual a média de duração dos filmes?
+5. Quantos filmes há por década?
+6. Qual gênero é mais frequente?
+7. Quais os filmes com mais de 3 horas de duração?
+8. Quais são os 10 melhores filmes de acordo com as avaliações? E os 10
+piores?
+'''
+
+import pandas as pd
+
+df = pd.read_csv('capacitacao_senai_python_daniella_torelli/22_05/csv/imdb_top_250_movies.csv', sep=',', encoding='utf-8', parse_dates=['year'])
+
+# Exercise 1
+
+counts_directors = df['directors'].value_counts()
+
+print(counts_directors)
+print('--------------------------------------------------')
+print(counts_directors.head(5))
+print('--------------------------------------------------')
+
+# Exercise 2
+
+print(f'Média das avaliações dos filmes: {df['rating'].mean():.2f}')
+print('--------------------------------------------------')
+
+# Exercise 3
+
+years_ascends = df.sort_values('year')
+print('5 Filmes mais antigos:')
+print(years_ascends.head(5)['name'])
+
+print('--------------------------------------------------')
+
+print('5 Filmes mais novos:')
+print(years_ascends.tail(5)['name'])
+
+print('--------------------------------------------------')
+
+# Exercise 4
+
+df['duration'] = df['run_time'].str.extract(r'(\d+)h').astype(float) * 60 + df['run_time'].str.extract(r'(\d+)m').astype(float)
+
+average = df['duration'].mean()
+
+print(f'A média dos filmes é: {average:.2f} minutos ou {(average / 60):.2f} horas')
+print('--------------------------------------------------')
+
+# Exercise 5 
+
+df['decade'] = (df['year'].dt.year // 10) * 10
+
+print(df['decade'].value_counts())
+print('--------------------------------------------------')
+
+# Exercise 6
+
+print(df['genre'].mode())
+print('--------------------------------------------------')
+
+# Exercise 7
+
+print('Filmes com m ais de 3 horas (180 minutos):')
+print(df.loc[df['duration'] > 180]['name'])
+print('--------------------------------------------------')
+
+# Exercise 8
+
+top = df.groupby('name')['rating'].max()
+top10 = top.sort_values(ascending = False).head(10)
+not_top10 = top.sort_values(ascending = False).tail(10)
+
+print('10 Filmes mais avaliados:')
+print(top10)
+
+print('--------------------------------------------------')
+
+print('10 Filmes menos avaliados:')
+print(not_top10)
