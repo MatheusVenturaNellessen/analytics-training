9b4c5d51af22b39a07afd6cd95dccf61b9d1708a
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2025-06-02 10:42:02 -0300

    feat: adiciona dashboards interativos para manipulacão de sistema_academia.bd

--- capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/sistema_academia.py
+++ capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/sistema_academia.py
@@ -93,28 +93,42 @@ conn.commit()
 
 # Popula o banco de dados SQLite
 
-cursor.execute('PRAGMA foreign_keys = ON;') # Permite utilização de chaves estrangeiras
+cursor.execute('PRAGMA foreign_keys = ON;')
 
-df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/exercicios.csv') # Lê o arquivo exercicios.csv e salva em um DataFrame
-df_exercicios.to_sql('exercicios', conn, if_exists='append', index=False) # Popula  a tebla "exercicios" do banco de dados SQLite
+cursor.execute("SELECT COUNT(*) FROM exercicios")
+if cursor.fetchone()[0] == 0:
+    df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/exercicios.csv')
+    df_exercicios.to_sql('exercicios', conn, if_exists='append', index=False)
 
-df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/instrutores.csv')
-df_exercicios.to_sql('instrutores', conn, if_exists='append', index=False)
+cursor.execute("SELECT COUNT(*) FROM instrutores")
+if cursor.fetchone()[0] == 0:
+    df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/instrutores.csv')
+    df_exercicios.to_sql('instrutores', conn, if_exists='append', index=False)
 
-df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/planos.csv')
-df_exercicios.to_sql('planos', conn, if_exists='append', index=False)
+cursor.execute("SELECT COUNT(*) FROM planos")
+if cursor.fetchone()[0] == 0:
+    df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/planos.csv')
+    df_exercicios.to_sql('planos', conn, if_exists='append', index=False)
 
-df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/clientes_academia.csv')
-df_exercicios.to_sql('clientes_academia', conn, if_exists='append', index=False)
+cursor.execute("SELECT COUNT(*) FROM clientes_academia")
+if cursor.fetchone()[0] == 0:
+    df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/clientes_academia.csv')
+    df_exercicios.to_sql('clientes_academia', conn, if_exists='append', index=False)
 
-df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/pagamento_clientes.csv')
-df_exercicios.to_sql('pagamento_clientes', conn, if_exists='append', index=False)
+cursor.execute("SELECT COUNT(*) FROM pagamento_clientes")
+if cursor.fetchone()[0] == 0:
+    df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/pagamento_clientes.csv')
+    df_exercicios.to_sql('pagamento_clientes', conn, if_exists='append', index=False)
 
-df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/treinos.csv')
-df_exercicios.to_sql('treinos', conn, if_exists='append', index=False)
+cursor.execute("SELECT COUNT(*) FROM treinos")
+if cursor.fetchone()[0] == 0:
+    df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/treinos.csv')
+    df_exercicios.to_sql('treinos', conn, if_exists='append', index=False)
 
-df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/treino_exercicios.csv')
-df_exercicios.to_sql('treino_exercicios', conn, if_exists='append', index=False)
+cursor.execute("SELECT COUNT(*) FROM treino_exercicios")
+if cursor.fetchone()[0] == 0:
+    df_exercicios = pd.read_csv('capacitacao_senai_python_daniella_torelli/30_05_to_04_06/project/csv/treino_exercicios.csv')
+    df_exercicios.to_sql('treino_exercicios', conn, if_exists='append', index=False)
 
 # 3. Criar uma aplicação Streamlit para:
 # 	3.1. Listar clientes e seus planos;
@@ -124,6 +138,8 @@ df_exercicios.to_sql('treino_exercicios', conn, if_exists='append', index=False)
 # 	3.5. Formulário para cadastro de clientes, pagamentos, treinos e exercícios nos treinos;
 # 	3.6. EXTRA: Usar a função de autenticação do streamlit para criar um login e senha.
 
+# Exercício 3.3
+
 st.subheader('Pagamentos por Clientes', divider=True)
 
 df_nomes_clientes = pd.read_sql('''
@@ -132,16 +148,18 @@ df_nomes_clientes = pd.read_sql('''
     conn
 )
 
-nomes_clientes = df_nomes_clientes['nome'].tolist()
+nomes_clientes_dict = {
+    f"{row['nome']} (ID {row['id']})": row['id']
+    for _, row in df_nomes_clientes.iterrows()
+}
 
-st.sidebar.header('Clientes')
+cliente_selecionado = st.selectbox(
+    'Selecione um cliente:',
+    options=list(nomes_clientes_dict.keys())
+)
 
-cliente_selecionado = st.sidebar.selectbox('Selecione um cliente:', nomes_clientes)
-
-# Recupera o ID do cliente selecionado
-cliente_selecionado_id = df_nomes_clientes.loc[
-    df_nomes_clientes['nome'] == cliente_selecionado, 'id'
-].iloc[0]
+# Recupera o ID correspondente
+cliente_selecionado_id = nomes_clientes_dict[cliente_selecionado]
 
 # 5.1) Conta quantos pagamentos esse cliente já fez
 cursor.execute(
@@ -168,3 +186,77 @@ if ultimo_pagamento:
     st.write(f'Seu último pagamento foi de **R$ {valor:.2f}**, em **{data}**.')
 else:
     st.write('Ainda não há pagamentos registrados para este cliente.')
+
+# Exercício 3.4
+
+st.subheader('Quantidade de Clientes por Instrutor', divider=True)
+
+df_nomes_instrutor = pd.read_sql_query('''
+    SELECT id, nome FROM instrutores
+    ''',
+    conn
+)
+
+nomes_instrutor_dict = {
+    f"{row['nome']} (ID {row['id']})": row['id']
+    for _, row in df_nomes_instrutor.iterrows()
+}
+
+instrutor_selecionado = st.selectbox(
+    'Selecione um instrutor:',
+    options=list(nomes_instrutor_dict.keys())
+)
+
+instrutor_selecionado_id = nomes_instrutor_dict[instrutor_selecionado]
+
+cursor.execute(
+    '''
+    SELECT COUNT(DISTINCT cliente_id)
+    FROM treinos
+    WHERE instrutor_id = ?
+    ''',
+    (instrutor_selecionado_id,)
+)
+total_alunos = cursor.fetchone()[0]
+
+st.write(f'O instrutor **{instrutor_selecionado}** possui **{total_alunos}** alunos.')
+
+
+# =============================
+#       Exercício Extra
+# =============================
+
+import hashlib
+
+# Criação da tabela "usario"
+cursor.execute('''
+	CREATE TABLE IF NOT EXISTS usuario (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    senha TEXT NOT NULL
+	)
+	''')
+conn.commit()
+
+def hash_senha(senha):
+    return hashlib.sha256(senha.encode()).hexdigest() # Retorna o SHA da senha
+
+# Popula tabela "usuario"
+cursor.execute("SELECT COUNT(*) FROM usuarios")
+if cursor.fetchone()[0] == 0:
+    usuarios = [
+        ("Alice Santos", "alice@email.com", "senha123"),
+        ("Bruno Silva", "bruno@email.com", "segredo"),
+        ("Carlos Lima", "carlos@email.com", "academia"),
+        ("Diana Costa", "diana@email.com", "fitness"),
+        ("Eduarda Reis", "eduarda@email.com", "malhacao")
+    ]
+    for nome, email, senha in usuarios:
+
+        senha_hash = hash_senha(senha)
+
+        cursor.execute("INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)",
+                        (nome, email, senha_hash))
+    conn.commit()
+
